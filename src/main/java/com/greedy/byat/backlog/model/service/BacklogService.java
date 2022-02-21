e77046e1db19a10f0b5948d15ff94f85ad9b6d21
ir2ver <ir2ver@gmail.com>
2022-02-21 17:32:10 +0900

    Revert "Revert "type : feat, docs, style""
    This reverts commit 640815db4f804368bb240d4967d51b1efa2e7ed9.

--- src/main/java/com/greedy/byat/backlog/model/service/BacklogService.java
+++ src/main/java/com/greedy/byat/backlog/model/service/BacklogService.java
@@ -3,8 +3,19 @@ package com.greedy.byat.backlog.model.service;
 import java.util.List;
 
 import com.greedy.byat.backlog.model.dto.BacklogDTO;
+import com.greedy.byat.common.exception.backlog.BacklogModifyException;
+import com.greedy.byat.common.exception.backlog.BacklogRegistException;
+import com.greedy.byat.common.exception.backlog.BacklogRemoveException;
 
 public interface BacklogService {
 
-	List<BacklogDTO> selectBacklogList(int projectCode);
+	List<BacklogDTO> selectAllBacklogList(int projectCode);
+	
+	void registBacklog(BacklogDTO backlog) throws BacklogRegistException;
+	
+	BacklogDTO selectBacklogDetail(int code);
+	
+	void modifyBacklog(BacklogDTO backlog) throws BacklogModifyException;
+	
+	void removeBacklog(int code) throws BacklogRemoveException;
 }
---
+++ src/main/java/com/greedy/byat/common/exception/backlog/BacklogRemoveException.java
@@ -0,0 +1,10 @@
+package com.greedy.byat.common.exception.backlog;
+
+public class BacklogRemoveException extends Exception {
+	
+	public BacklogRemoveException() {}
+	
+	public BacklogRemoveException(String msg) {
+		super(msg);
+	}
+}
