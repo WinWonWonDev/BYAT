ec4c5f7149b6fd6ecd04945b4fcd8c5e9606ebf4
soo <qkrtnqls5669@gmail.com>
2022-03-03 00:52:53 +0900

    type  :  docs
    comment :
    
    BYAT 프로젝트 meetinglog 코드수정( 목록조회 진행중)

--- src/main/java/com/greedy/byat/meetinglog/model/service/MeetinglogServiceImpl.java
+++ src/main/java/com/greedy/byat/meetinglog/model/service/MeetinglogServiceImpl.java
@@ -1,9 +1,12 @@
 package com.greedy.byat.meetinglog.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.greedy.byat.meetinglog.model.dao.MeetinglogMapper;
+import com.greedy.byat.meetinglog.model.dto.MeetinglogDTO;
 
 @Service
 public class MeetinglogServiceImpl implements MeetinglogService {
@@ -14,6 +17,15 @@ public class MeetinglogServiceImpl implements MeetinglogService {
 	public MeetinglogServiceImpl(MeetinglogMapper mapper) {
 		this.mapper = mapper;
 	}
+
+	@Override
+	public List<MeetinglogDTO> selectMeetinglogList(int projectCode) {
+
+		List<MeetinglogDTO> meetinglog = mapper.selectMeetinglogList(projectCode);
+		
+		return meetinglog;
+	}
+
 
 	
 
