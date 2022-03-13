b7cf3a62a9f1bbf6fd8ee9a453ef3cb23e26c60b
ir2ver <ir2ver@gmail.com>
2022-03-13 12:40:06 +0900

    type : fix, docs
    comment : BYAT프로젝트 backlog, profile, authInterceptor 문서화 주석 수정

--- src/main/java/com/greedy/byat/common/filter/model/service/MenuServiceImpl.java
+++ src/main/java/com/greedy/byat/common/filter/model/service/MenuServiceImpl.java
@@ -7,6 +7,19 @@ import org.springframework.stereotype.Repository;
 
 import com.greedy.byat.common.filter.model.dao.MenuMapper;
 
+/**
+ * <pre>
+ * Class : MenuServiceImpl
+ * Comment : MenuService를 상속받아 method들을 재정의한 Class
+ * History
+ * 2021/03/08 (황인수) 처음 작성함
+ * 2021/03/09 (황인수) postHandle 부분 보완
+ * 2021/03/10 (황인수) postHandle DB 연결 성공
+ * </pre>
+ * @version 3
+ * @author 황인수
+ * @see MenuController, MenuService, MenuMapper
+ * */
 @Repository
 public class MenuServiceImpl implements MenuService {
 	
